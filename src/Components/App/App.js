3e9298c2edb48c627debac92a82039078764f3df
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-19 15:01:11 -0500

    add background color to nav

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Switch, //put switch around the routes will check all the routes for a match, then if not it will render the 404--see last route before </switch>
+} from 'react-router-dom';
+
+//components
+import Nav from '../Nav/Nav';
+
+//routes
+import Home from '../Home/Home';
 
 class App extends Component {
   render() {
     return (
-      <>
-      <h1>Hello World</h1>
-      </>
+      <Router>
+        <Nav />
+        <div>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={Home}
+            />
+            <Route render={() => <center><h1>404<br /><br />This page doesn't exist<br />Why did you come here?</h1></center>} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+class Nav extends Component {
+    render() {
+        return (
+            <header>
+                <center>
+                    <h1>Trevor Ramlow</h1>
+                </center>
+            </header>
+        );
+    }
+}
+
+export default Nav;
