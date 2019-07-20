cdda3d58a861d3df60823b89c77b36f339771a27
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-20 12:38:33 -0500

    add a gif to the home screen

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Trevor from './trevor.jpg';
+import soloGif from './solo-gif.gif';
 
 class Home extends Component {
   render() {
     return (
       <>
       <center>
-      <img className="profile-picture" src={Trevor} />
+      <img className="profile-picture" src={Trevor} /><br />
+      <p><i>
+        This is my about me
+        </i></p><br />
+        <div className="projects">
+          <img src={soloGif} />
+        </div>
       </center>
       </>
     );
