cd44961dd8e40e50aed58a469c20d152df43bb80
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-25 16:19:29 -0500

    reorganize file structure and update styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import Trevor from './trevor.jpg';
+import Trevor from '../images/trevor.jpg';
 
 //gif
-import soloGif from './solo-gif.gif';
-import feedbackGif from './feedback-gif.gif';
+import soloGif from '../images/solo-gif.gif';
+import feedbackGif from '../images/feedback-gif.gif';
 
 //styling
 import AppBar from '@material-ui/core/AppBar';
@@ -33,7 +33,7 @@ class Home extends Component {
             <div className="solo-project">
               <label>Solo Project - Card Collection Tracker</label><br />
               <img src={soloGif} />
-              <br /><br />
+              {/* <br /><br /> */}
               <p>This is a p box that describes what the project is about. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut nisi in ligula molestie dictum sit amet non enim. Mauris varius accumsan neque, nec sollicitudin diam tincidunt et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam malesuada, lorem ac porttitor lacinia, ipsum diam consequat tellus, a rutrum ligula eros tempor lacus. Mauris mi velit, cursus sed facilisis sagittis, maximus nec quam. Duis venenatis ipsum eget nisi ultricies rhoncus. Morbi feugiat nunc id lacus ullamcorper aliquet. Nam venenatis nisi metus, quis pulvinar lacus consectetur a. Nullam id massa orci. Etiam sem erat, ornare eu velit id, venenatis aliquam libero. Integer vulputate sem et est facilisis, id mattis est congue. Mauris porttitor id diam sed bibendum. Donec sodales ornare nibh, sit amet scelerisque purus ullamcorper vitae. Morbi at ligula id nisi tincidunt fringilla. Vivamus ac vulputate sem. </p>
             </div>
             <div className="feedback-project">
@@ -44,6 +44,7 @@ class Home extends Component {
             </div>
           </div>
           <footer>
+            <br /><br />
           </footer>
         </center>
       </>
