77cef0a21992c45eda3e87b6156e9f97b0cb83db
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-31 12:45:54 -0500

    added info to about me and contact section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,9 +10,9 @@ import feedbackGif from '../images/feedback-gif.gif';
 class Home extends Component {
 
   state = {
-   display: 'About Me',
+    display: 'About Me',
   }
-  
+
   handleClick = (string) => {
     console.log('hello')
     this.setState({
@@ -29,23 +29,23 @@ class Home extends Component {
           <div className="links">
             <h2>
               <ul className="nav-list">
-                <li onClick={() => {this.handleClick('About Me')}}><u>About Me</u></li>
-                <li onClick={() => {this.handleClick('Projects')}}><u>Projects</u></li>
-                <li onClick={() => {this.handleClick('Contact')}}><u>Contact</u></li>
+                <li onClick={() => { this.handleClick('About Me') }}><u>About Me</u></li>
+                <li onClick={() => { this.handleClick('Projects') }}><u>Projects</u></li>
+                <li onClick={() => { this.handleClick('Contact') }}><u>Contact</u></li>
               </ul>
-              </h2>
+            </h2>
           </div>
           <br />
           <div className="project-grid">
             {this.state.display === 'Projects' ?
               <>
                 <div className="projects">
-                  <label>Solo Project - Card Collection Tracker</label><br />
+                  <h3>Solo Project - Card Collection Tracker</h3><br />
                   <img src={soloGif} alt="A gif of the Card Collection Tracker app" />
                   <p>This is a p box that describes what the project is about. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut nisi in ligula molestie dictum sit amet non enim. Mauris varius accumsan neque, nec sollicitudin diam tincidunt et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam malesuada, lorem ac porttitor lacinia, ipsum diam consequat tellus, a rutrum ligula eros tempor lacus. Mauris mi velit, cursus sed facilisis sagittis, maximus nec quam. Duis venenatis ipsum eget nisi ultricies rhoncus. Morbi feugiat nunc id lacus ullamcorper aliquet. Nam venenatis nisi metus, quis pulvinar lacus consectetur a. Nullam id massa orci. Etiam sem erat, ornare eu velit id, venenatis aliquam libero. Integer vulputate sem et est facilisis, id mattis est congue. Mauris porttitor id diam sed bibendum. Donec sodales ornare nibh, sit amet scelerisque purus ullamcorper vitae. Morbi at ligula id nisi tincidunt fringilla. Vivamus ac vulputate sem. </p>
                 </div>
                 <div className="projects">
-                  <label>Daily Feedback Form</label><br />
+                  <h3>Daily Feedback Form</h3><br />
                   <img src={feedbackGif} alt="A gif of the Daily Feedback Form app" />
                   <p>This is a p box that describes what the project is about. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut nisi in ligula molestie dictum sit amet non enim. Mauris varius accumsan neque, nec sollicitudin diam tincidunt et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam malesuada, lorem ac porttitor lacinia, ipsum diam consequat tellus, a rutrum ligula eros tempor lacus. Mauris mi velit, cursus sed facilisis sagittis, maximus nec quam. Duis venenatis ipsum eget nisi ultricies rhoncus. Morbi feugiat nunc id lacus ullamcorper aliquet. Nam venenatis nisi metus, quis pulvinar lacus consectetur a. Nullam id massa orci. Etiam sem erat, ornare eu velit id, venenatis aliquam libero. Integer vulputate sem et est facilisis, id mattis est congue. Mauris porttitor id diam sed bibendum. Donec sodales ornare nibh, sit amet scelerisque purus ullamcorper vitae. Morbi at ligula id nisi tincidunt fringilla. Vivamus ac vulputate sem. </p>
                 </div>
@@ -55,22 +55,42 @@ class Home extends Component {
               </>
             }
             {this.state.display === 'About Me' ?
-              <>
-                <h3>About Me</h3>
-              </>
+              <div className="about-me">
+                <h3>About Me</h3><br />
+                Nice to meet you! My name is Trevor, and I enjoy a variety of hobbies. I actively follow Magic tthe Gathering tournaments, play a variety of board games, and enjoy traveling with my girlfriend to go see bands. Aside from this, I have recently developed a passion for coding and have decided to pursue this at <a style={{ color: "grey" }} href="https://primeacademy.io/">Prime Digital Academy</a>. Here I have been able to learn more than I ever imagined, and I haven't even scratched the surface on everything there is out there. Here we have focused on learning full-stack development. Some of the languages, frameworks, and programs we’ve used are:
+                <br />
+                <br /><div className="skill-list">
+                  <li>JavaScript</li>
+                  <li>jQuery</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Node.js</li>
+                  <li>PostgreSQL</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Express</li>
+                  <li>Material-UI</li>
+                  <li>Postman</li>
+                  <li>GitHub</li>
+                </div><br />
+                <br />
+                If you're interested in looking at a sample of projects I have done, you can click the Projects header to learn more. If you would like to reach out to me, click the Contact header. 
+              </div>
               :
               <>
               </>
             }
-             {this.state.display === 'Contact' ?
-              <>
+            {this.state.display === 'Contact' ?
+              <div className="contact">
                 <h3>Contact</h3>
-              </>
+                The best method to reach me is via email. You can send any inquiries to:<br />
+                <a href="mailto:trevor.ramlow@gmail.com">trevor.ramlow@gmail.com</a>
+              </div>
               :
               <>
               </>
             }
-            
+
           </div>
           <footer>
             <br /><br />
