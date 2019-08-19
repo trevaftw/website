0c2742faf017a58e912e35281c2cf6a313e2da1d
Trevor Ramlow <trevorramlow@gmail.com>
2019-08-19 15:11:20 -0500

    removed favico

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,7 +4,7 @@ class AboutMe extends Component {
     render() {
         return (
             <div className="about-me">
-                <p className="about-me-info">Nice to meet you! My name is Trevor, and I enjoy a variety of hobbies. I actively follow Magic tthe Gathering tournaments, play a variety of board games, and enjoy traveling with my girlfriend to go see bands. Aside from this, I have recently developed a passion for coding and have decided to pursue this at <a style={{ color: "black" }} href="https://primeacademy.io/">Prime Digital Academy</a>. Here I have been able to learn more than I ever imagined, and I haven't even scratched the surface on everything there is out there. Here we have focused on learning full-stack development. Some of the languages, frameworks, and programs we’ve used are: </p>
+                <p>Nice to meet you! My name is Trevor, and I enjoy a variety of hobbies. I actively follow Magic tthe Gathering tournaments, play a variety of board games, and enjoy traveling with my girlfriend to go see bands. Aside from this, I have recently developed a passion for coding and have decided to pursue this at <a style={{ color: "black" }} href="https://primeacademy.io/">Prime Digital Academy</a>. Here I have been able to learn more than I ever imagined, and I haven't even scratched the surface on everything there is out there. Here we have focused on learning full-stack development. Some of the languages, frameworks, and programs we’ve used are: </p>
                 <div className="skill-list">
                     <li>JavaScript</li>
                     <li>jQuery</li>
@@ -22,7 +22,7 @@ class AboutMe extends Component {
                     <li>twilio</li>
                     <li>Nodemailer</li>
                 </div>
-                <p className="about-me-info">If you're interested in looking at a sample of projects I have done, you can click the Projects header to learn more. If you would like to reach out to me, click the Contact header.</p>
+                <p>If you're interested in looking at a sample of projects I have done, you can click the Projects header to learn more. If you would like to reach out to me, click the Contact header.</p>
             </div>
         )
     }
